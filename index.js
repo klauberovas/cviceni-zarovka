714ff182b27f0511c4504f443dbb41941dac8d8f
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-09-30 13:35:32 +0200

    Done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('funguju!');
+
+const bulbElm = document.querySelector('.bulb');
+
+const bulbOnOffElm = () => {
+  bulbElm.classList.toggle('bulb--on');
+};
+
+bulbElm.addEventListener('keydown', bulbOnOffElm);
